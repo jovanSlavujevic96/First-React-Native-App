09cd221fdb3bdb782c8121d8d67aa55d2b20999a
Jovan Slavujevic <slavujevic.jovan.96@gmail.com>
2023-11-20 22:22:37 +0100

    Added contacts

--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+
+import { contacts } from '../config/data';
+import colors from '../config/colors';
+
+
+const Item = ({contact}) => {
+  const name = contact.name;
+  return (
+    <View>
+        <Text >Email: {contact.email}</Text>
+        <Text >Name: {name.title + " " + name.first + " " + name.last}</Text>
+        <Text/>
+    </View>
+  );
+}
 
 class Contacts extends Component {
   render() {
@@ -7,7 +22,13 @@ class Contacts extends Component {
       <View
         style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
       >
-        <Text>Contacts Screen</Text>
+        <Text style={{ color:"red"}}>Contacts Screen</Text>
+        <FlatList
+          style={{backgroundColor: colors.background}}
+          data={contacts}
+          renderItem={({item}) => <Item contact={item} />}
+          keyExtractor={(item) => item.email}
+        />
       </View>
     );
   }
