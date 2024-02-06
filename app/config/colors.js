7008e96707ba996e5c0ab3cec33bf193d50d4852
Jovan Slavujevic <slavujevic.jovan.96@gmail.com>
2024-02-07 00:51:29 +0100

    Added contact details - Homework

--- app/config/colors.js
+++ app/config/colors.js
@@ -3,4 +3,5 @@ export default {
   primaryText: '#525252',
   subtleText: '#9a9a9a',
   rowUnderlay: 'rgba(154, 154, 154, 0.25)',
+  rowLight: 'rgba(154, 154, 154, 0.15)',
 };
--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+
+import {Header} from '../components/UserDetails';
+import colors from '../config/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const DataRow = ({typeOfData, valueOfData, iconName}) => {
+  const isArray = Array.isArray(iconName);
+  const iconNames = (isArray) ? iconName : [iconName];
+  return(
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+      <View>
+        <Text style={{ color: colors.subtleText }}>{`${typeOfData}`}</Text>
+        <Text style={{ fontSize: 20, color: colors.primaryText }}>{`${valueOfData}`}</Text>
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
+        {iconNames.map((item, index) => (
+          <Icon
+            key={index}
+            name={`${item}`}
+            size={30}
+            color="blue"
+            style={{ alignSelf: 'flex-end', margin: 3 }}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
 
 class Details extends Component {
   render() {
+    const contact = this.props.route.params;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>{`${this.props.route.params.name.first}`}</Text>
-      </View>
+      <ScrollView style={{ backgroundColor: colors.background }}>
+        <Header {...contact} />
+        <View style={{ padding: 20, backgroundColor: colors.rowLight }}>
+          <DataRow typeOfData="email" valueOfData={contact.email} iconName="mail"/>
+          <DataRow typeOfData="cell" valueOfData={contact.cell} iconName={["call", "chatbox-ellipses"]}/>
+          <DataRow typeOfData="home" valueOfData={contact.phone} iconName="call"/>
+        </View>
+      </ScrollView>
     );
   }
 }
